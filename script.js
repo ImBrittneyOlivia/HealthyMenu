17b50427da1b02892252c2b24eae49edfd075cdd
education.devs <education.devs@gmail.com>
2018-09-25 17:35:04 -0700

    final project

---
+++ script.js
@@ -0,0 +1,4 @@
+$("div").on("click", function() {
+  $(this).toggleClass("show-description");
+});
+/* selects div elements --when clicked on listen for event (the click) if that happens we want to toggleClass show description*/
